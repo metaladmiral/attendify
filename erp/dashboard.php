20e4e3d47a523526f1d6a721ed18fdb4a53896a5
Prakhar Sinha <thesecretinspiration@gmail.com>
2023-07-06 11:15:42 +0530

    Fix: table s.no

--- erp/dashboard.php
+++ erp/dashboard.php
@@ -215,7 +215,7 @@ foreach ($batchLabelData as $key => $value) {
             let html = "";
             for(const key in data) {
                 html += `<tr>
-                <td>${key+1}</td><td>${data[key].name}</td>
+                <td>${parseInt(key)+1}</td><td>${data[key].name}</td>
                 <td>${data[key].marks.phase1.mst}</td>
                 <td>${data[key].marks.phase1.assign}</td>
                 <td>${data[key].marks.phase2.mst}</td>
