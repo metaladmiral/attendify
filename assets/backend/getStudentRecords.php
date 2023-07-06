a09b65979e5e5c78d4ff1943442f443ba18cd392
Prakhar Sinha <thesecretinspiration@gmail.com>
2023-07-06 08:36:08 +0530

    Fix: students now can be searched using batch only (section not required)

--- assets/backend/getStudentRecords.php
+++ assets/backend/getStudentRecords.php
@@ -6,8 +6,13 @@ require_once 'conn.php';
 $conn = new Db;
 
 $offset = $_POST['offset'];
+if($_POST['sectionid']!='null') {
+    $sql = $conn->mconnect()->prepare("SELECT studid, name, marks, totalattendance FROM `students` WHERE `batchid`='".$_POST['batchid']."' AND `sectionid`='".$_POST['sectionid']."' ");
+}
+else {
+    $sql = $conn->mconnect()->prepare("SELECT studid, name, marks, totalattendance FROM `students` WHERE `batchid`='".$_POST['batchid']."' ");
+}
 
-$sql = $conn->mconnect()->prepare("SELECT studid, name, marks, totalattendance FROM `students` WHERE `batchid`='".$_POST['batchid']."' AND `sectionid`='".$_POST['sectionid']."' ");
 $sql->execute();
 $data = $sql->fetchAll(PDO::FETCH_ASSOC);
 
