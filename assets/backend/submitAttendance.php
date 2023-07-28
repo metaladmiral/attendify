f13ec63aa9395dc538b1f8aaacb9873ea74172c2
Prakhar Sinha <thesecretinspiration@gmail.com>
2023-07-29 01:13:58 +0530

    Fix: submit Attendance working now

--- assets/backend/submitAttendance.php
+++ assets/backend/submitAttendance.php
@@ -31,21 +31,30 @@ catch(PDOException $e) {
     }
 }
     
-    try {
-        $sql = $conn->mconnect()->prepare("UPDATE `$tablename` SET absentStudents=? WHERE `date`='$date' ");
-        $sql->execute();
+    $sql = $conn->mconnect()->prepare("SELECT count(*) as c FROM `$tableName` WHERE `date`='$date' AND `sectionid`='$sectionId' AND `subjectid`='$subjectId' ");
+    $sql->execute();
+    $count = $sql->fetch(PDO::FETCH_COLUMN);
+    if($count) {
+        try {
+            $sql = $conn->mconnect()->prepare("UPDATE `$tableName` SET absentStudents=? WHERE `date`='$date' AND `sectionid`='$sectionId' AND `subjectid`='$subjectId' ");
+            $sql->execute([$absentStudents]);
+            $_SESSION["succ"] = "1";
+            header('Location: ../../erp/mark-attendance.php');
+        } catch (PDOException $e) {
+            $_SESSION["succ"] = "0";
+            header('Location: ../../erp/mark-attendance.php');
+        }
     }
-    catch(PDOException $e) {
+    else {
         try {
             $sql = $conn->mconnect()->prepare("INSERT INTO `$tableName`(sectionid, subjectid, date, absentStudents) VALUES(?, ?, ?, ?) ");
             $sql->execute([$sectionId, $subjectId, $date, $absentStudents]);
             $_SESSION["succ"] = "1";
             header('Location: ../../erp/mark-attendance.php');
         }
-        catch(PDOException $e) {
-            echo $e->getMessage();
+        catch(PDOException $ef) {
+            // echo $ef->getMessage();
             $_SESSION["succ"] = "0";
             header('Location: ../../erp/mark-attendance.php');
-        
         }
     }
