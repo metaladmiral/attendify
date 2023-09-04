15466b12bc6219ede7c00a9de98a0162efece608
Prakhar Sinha <thesecretinspiration@gmail.com>
2023-09-04 10:09:50 +0530

    Fix: footer

--- erp/footer.php
+++ erp/footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row align-items-center flex-row-reverse">
             <div class="col-md-12 col-sm-12 text-center">
-                <span id="year"></span> © SGL ERP
+                <span id="year"></span> © CGC CMS
             </div>
         </div>
     </div>
