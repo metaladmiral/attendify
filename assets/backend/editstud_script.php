a03ef719fb98361b33f2d7e65aaeefe79ee40372
Prakhar Sinha <thesecretinspiration@gmail.com>
2023-09-18 05:51:36 +0530

    Feat: edit student

--- assets/backend/editstud_script.php
+++ assets/backend/editstud_script.php
@@ -5,25 +5,38 @@ require_once 'conn.php';
 // var_dump($_POST);
 $conn = new Db;
 
-$studid = $_POST['studid'];
-$marks = array(
-    "phase1"=>array("mst"=>$_POST['mst1'], "assign"=>$_POST['assign1']),
-    "phase2"=>array("mst"=>$_POST['mst2'], "assign"=>$_POST['assign2'])
-);
+// die();
 
-$totalAtt = $_POST['totalattendance'];
-$marks = json_encode($marks);
+$studid = $_POST['studid'];
+
+$hosteldetails = json_encode(array(
+    "hosteler"=> $_POST['hosteler'],
+    "roomno"=> $_POST['roomno'],
+    "hostelname"=> $_POST['hostelname']
+));
+
+$parentworkdetails = json_encode(array(
+    "parentoccupation" => $_POST['parentoccupation'],
+    "parannualincome" => $_POST['parannualincome']
+));
+$loandetails = json_encode(array(
+    "loanstatus" => $_POST['loanstatus'],
+    "loanamount" => $_POST['loanamount']
+));
+$marksinschool = json_encode(array($_POST['10thmarks'], $_POST['12thmarks']));
+$natureofstud = json_encode(array_values($_POST['natureofstud']));
+
 
 try {
-    $sql = "UPDATE `students` SET `marks`=?, `totalattendance`=? WHERE `studid`='$studid' ";
+    $sql = "UPDATE `students` SET `sectionid`='".$_POST['section']."', `name`='".$_POST['name']."', `semester`='".$_POST['sem']."', `bloodgrp`='".$_POST['bldgrp']."', `dob`='".$_POST['dob']."', `dep`='".$_POST['depname']."', `classroll`='".$_POST['classroll']."', `uniroll`='".$_POST['uniroll']."', `studemail`='".$_POST['studemail']."', `parentemail`='".$_POST['pemail']."', `fname`='".$_POST['fname']."', `mname`='".$_POST['mname']."', `category`='".$_POST['category']."', `mno`='".$_POST['mno']."', `mano`='".$_POST['mano']."', `wno`='".$_POST['wno']."', `permaddr`='".$_POST['permaddr']."', `localaddr`='".$_POST['localaddr']."', `state`='".$_POST['state']."', `district`='".$_POST['district']."', `hosteldetails`='$hosteldetails', `parentsworkdetails`='$parentworkdetails', `loandetails`='$loandetails', `unhealthyhabits`='".$_POST['unhealthyhabits']."', `marksinschool`='$marksinschool', `aimofedu`='".$_POST['aimoe']."', `personaltraits`='".$_POST['personaltraits']."', `natureofstudent`='$natureofstud', `initcommskill`='".$_POST['initcommskill']."' WHERE `studid`='$studid' ";
     $query = $conn->mconnect()->prepare($sql);
-    $query->execute([$marks, $totalAtt]);
-    // echo 1;
+    $query->execute();
+    echo 1;
     $_SESSION['message'] = "1";
     header('Location: '.$_SERVER['HTTP_REFERER']);
     
 }catch(PDOException $e) {
-    // echo $e->getMessage();
-    $_SESSION['message'] = "2";
-    header('Location: '.$_SERVER['HTTP_REFERER']);
+    echo $e->getMessage();
+    // $_SESSION['message'] = "2";
+    // header('Location: '.$_SERVER['HTTP_REFERER']);
 }
